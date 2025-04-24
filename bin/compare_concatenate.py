cb57c8a6a4a1e9c3c00da30745456a131519dd97
dltamayo <DLT@breakthroughcancer.org>
2025-04-24 15:29:45 -0400

    Add main to scripts

--- bin/compare_concatenate.py
+++ bin/compare_concatenate.py
@@ -8,59 +8,62 @@ Output: $concatenated_cdr3.txt
 
 # Import modules
 import argparse
-import glob
 import os
 import pandas as pd
 
-# Initialize the parser
-parser = argparse.ArgumentParser(description="Take positional args")
+def main():
+    # Initialize the parser
+    parser = argparse.ArgumentParser(description="Take positional args")
 
-# Add positional arguments
-parser.add_argument("data_dir")
-parser.add_argument("samplesheet")
+    # Add positional arguments
+    parser.add_argument("data_dir")
+    parser.add_argument("samplesheet")
 
-# Parse the arguments
-args = parser.parse_args()
+    # Parse the arguments
+    args = parser.parse_args()
 
-# Print the arguments
-print("data_dir: ", args.data_dir)
-print("samplesheet: ", args.samplesheet)
+    # Print the arguments
+    print("data_dir: ", args.data_dir)
+    print("samplesheet: ", args.samplesheet)
 
-samplesheet = pd.read_csv(args.samplesheet, header=0)
+    samplesheet = pd.read_csv(args.samplesheet, header=0)
 
-dfs = []
-for index, row in samplesheet.iterrows():
-    file_path = os.path.basename(row['file'])
-    file_path = os.path.join(args.data_dir, file_path)
-    print(f"Loading {file_path}")
-    
-    # Read the TSV file into a dataframe
-    df = pd.read_csv(file_path, sep="\t", header=0)
-    
-    # Get metadata
-    subject_id = row['subject_id']
-    timepoint = row['timepoint']
-    origin = row['origin']
+    dfs = []
+    for index, row in samplesheet.iterrows():
+        file_path = os.path.basename(row['file'])
+        file_path = os.path.join(args.data_dir, file_path)
+        print(f"Loading {file_path}")
         
-    # Add patient column
-    df['patient'] = f"{subject_id}:{timepoint}_{origin}"
-    df['sample'] = row['sample']
-    
-    # Select relevant columns
-    df = df[['aminoAcid', 'vGeneName', 'jGeneName', 'patient', 'count (templates/reads)', 'sample']]
-    dfs.append(df)
+        # Read the TSV file into a dataframe
+        df = pd.read_csv(file_path, sep="\t", header=0)
+        
+        # Get metadata
+        subject_id = row['subject_id']
+        timepoint = row['timepoint']
+        origin = row['origin']
+            
+        # Add patient column
+        df['patient'] = f"{subject_id}:{timepoint}_{origin}"
+        df['sample'] = row['sample']
+        
+        # Select relevant columns
+        df = df[['aminoAcid', 'vGeneName', 'jGeneName', 'patient', 'count (templates/reads)', 'sample']]
+        dfs.append(df)
 
 
-# Concatenate all the dataframes into one
-df_combined = pd.concat(dfs)
+    # Concatenate all the dataframes into one
+    df_combined = pd.concat(dfs)
 
-# Rename columns as required
-df_combined = df_combined.rename(columns={
-    'aminoAcid': 'CDR3b',
-    'vGeneName': 'TRBV',
-    'jGeneName': 'TRBJ',
-    'count (templates/reads)': 'counts'
-})
-df_combined = df_combined[df_combined['CDR3b'].notna()]
+    # Rename columns as required
+    df_combined = df_combined.rename(columns={
+        'aminoAcid': 'CDR3b',
+        'vGeneName': 'TRBV',
+        'jGeneName': 'TRBJ',
+        'count (templates/reads)': 'counts'
+    })
+    df_combined = df_combined[df_combined['CDR3b'].notna()]
 
-df_combined.to_csv(f"concatenated_cdr3.txt", sep="\t", index=False, header=True)
+    df_combined.to_csv(f"concatenated_cdr3.txt", sep="\t", index=False, header=True)
+
+if __name__ == "__main__":
+    main()
--- bin/samplesheet.py
+++ bin/samplesheet.py
@@ -1,24 +1,9 @@
 #!/usr/bin/env python
 
 import argparse
-import os
 import pandas as pd
 
-# initialize parser
-parser = argparse.ArgumentParser()
-parser.add_argument('-s', '--samplesheet', 
-                    metavar='samplesheet', 
-                    type=str, 
-                    help='sample metadata passed in through samples CSV file')
-
-parser.add_argument('-d', '--data_dir',
-                    metavar='data_dir',
-                    type=str,
-                    help='path to data directory')
-
-args = parser.parse_args()
-
-#do any processing of the samplesheet here
+# do any processing of the samplesheet here
 def samplesheet(samplesheet, data_dir):
     ss = pd.read_csv(samplesheet, sep=',')
     ss.to_csv('samplesheet_utf8.csv', index=False, encoding='utf-8-sig')
@@ -28,5 +13,22 @@ def samplesheet(samplesheet, data_dir):
     
     print(ss.head())
 
-samplesheet(args.samplesheet, args.data_dir)
-    
+def main():
+    # initialize parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--samplesheet', 
+                        metavar='samplesheet', 
+                        type=str, 
+                        help='sample metadata passed in through samples CSV file')
+
+    parser.add_argument('-d', '--data_dir',
+                        metavar='data_dir',
+                        type=str,
+                        help='path to data directory')
+
+    args = parser.parse_args()
+
+    samplesheet(args.samplesheet, args.data_dir)
+
+if __name__ == "__main__":
+    main()
